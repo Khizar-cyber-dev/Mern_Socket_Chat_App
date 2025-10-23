ea1f3c8d13dd591a3b06a17f67cff307d5fb7c18
Khizar-cyber-dev <hafizkhizarkhizar07@gmail.com>
2025-10-23 19:47:59 +0500

    working.....

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const SERVER_URL = import.meta.env.SERVER_URL;
+export const SERVER_URL = import.meta.env.VITE_SERVER_URL;
 
 const axiosInstance = axios.create({
 	baseURL: `${SERVER_URL}/api`,

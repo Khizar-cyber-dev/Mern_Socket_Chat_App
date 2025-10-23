dc0635a89bffef56e86749572696a763eb388788
Khizar-cyber-dev <hafizkhizarkhizar07@gmail.com>
2025-10-23 17:51:21 +0500

    adding env variable for client and fix package.json

--- backend/src/route/authRoute.js
+++ backend/src/route/authRoute.js
@@ -24,11 +24,11 @@ router.get("/google", passport.authenticate("google", { scope: ["profile", "emai
 
 router.get(
   "/google/callback",
-   passport.authenticate("google", { session: false, failureRedirect: "http://localhost:5173/login?error=google" }),
+   passport.authenticate("google", { session: false, failureRedirect: `${CLIENT_URL}/login?error=google` }),
   (req, res) => {
     const { accessToken, refreshToken } = generateToken({ id: req.user._id });
     setTokenCookies(res, accessToken, refreshToken);
-    res.redirect("http://localhost:5173/dashboard");
+    res.redirect(`${CLIENT_URL}/dashboard`);
   }
 );
 
@@ -37,11 +37,11 @@ router.get("/github", passport.authenticate("github", { scope: ["user:email"] })
 
 router.get(
   "/github/callback",
-  passport.authenticate("github", { session: false, failureRedirect: "http://localhost:5173/login?error=google" }),
+  passport.authenticate("github", { session: false, failureRedirect: `${CLIENT_URL}/login?error=github` }),
   (req, res) => {
     const { accessToken, refreshToken } = generateToken({ id: req.user._id });
     setTokenCookies(res, accessToken, refreshToken);
-    res.redirect("http://localhost:5173/dashboard");
+    res.redirect(`${CLIENT_URL}/dashboard`);
   }
 );
 
